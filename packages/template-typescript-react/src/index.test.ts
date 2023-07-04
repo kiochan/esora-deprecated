d2388c27a822d63a9d6d9cd89baff25ed95f1058
kiochan <kio@kiochan.one>
2023-07-04 14:43:46 +0200

    test(template-typescript-react): 💍 test default export

--- packages/template-typescript-react/src/index.test.ts
+++ packages/template-typescript-react/src/index.test.ts
@@ -1,8 +1,8 @@
 import * as packageExports from './index'
 
 describe('core package', () => {
-  test('should have a default export', (done) => {
-    expect(typeof packageExports.default).toBe('object')
+  test('should have a functional component as default export', (done) => {
+    expect(typeof packageExports.default).toBe('function')
     done()
   })
 })
